71c395f51fb8745e1b7a9595c15ad04750fd7a98
lucas <lucas@localhost>
2016-08-28 19:54:06 -0300

    Correções. Listando clientes de acordo com a posição GPS do BAM.

--- www/js/Controllers/HomeCtrl.js
+++ www/js/Controllers/HomeCtrl.js
@@ -1,6 +1,7 @@
 angular.module('starter')
 
-        .controller('HomeCtrl', function (ValidacaoModuloFactory, NavegacaoModuloFactory, $scope, $rootScope, StorageModuloFactory) {
+        .controller('HomeCtrl', function (ExtraModuloFactory, ValidacaoModuloFactory, NavegacaoModuloFactory, $scope, $rootScope, StorageModuloFactory) {
+            $scope.saudacao = ExtraModuloFactory.saudacao();
             $scope.user = StorageModuloFactory.local.getObject(StorageModuloFactory.enum.user);
             $scope.cliente = StorageModuloFactory.local.getObject(StorageModuloFactory.enum.pdvAtivo);
             if (ValidacaoModuloFactory.isNotNull($scope.cliente)) {
--- www/js/Modulos/ExtraModulo.js
+++ www/js/Modulos/ExtraModulo.js
@@ -46,15 +46,27 @@ angular.module('starter')
                         return scope;
                     };
 
-                    services.calculaDistance = function(lat1, lon1, lat2, lon2) {
-                      var R = 6371; // now in km (change for get miles)
-                      var dLat = (lat2-lat1) * Math.PI / 180;
-                      var dLon = (lon2-lon1) * Math.PI / 180;
-                      var a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(lat1 * Math.PI / 180 ) * Math.cos(lat2 * Math.PI / 180 ) * Math.sin(dLon/2) * Math.sin(dLon/2);
-                      var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-                      var d = (R * c) || 0;
-                      return d;
-                    }
+                    services.calculaDistance = function (lat1, lon1, lat2, lon2) {
+                        var R = 6371; // now in km (change for get miles)
+                        var dLat = (lat2 - lat1) * Math.PI / 180;
+                        var dLon = (lon2 - lon1) * Math.PI / 180;
+                        var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+                        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+                        var d = (R * c) || 0;
+                        return d;
+                    };
+
+                    services.saudacao = function () {
+                        var data = new Date();
+                        var hora = data.getHours();
+
+                        if (hora < 12)
+                            return 'Bom dia';
+                        else if (hora >= 12 && hora < 18)
+                            return 'Bom tarde';
+                        else if (hora >= 18 && hora < 24)
+                            return 'Bom noite';
+                    };
 
                     return services;
                 }
