3d4b1ded79b49aefdaaff5d47de0156e4ec5bb9a
lucas <lucas@localhost>
2016-09-01 21:55:37 -0300

    Correções. Listando clientes de acordo com a posição GPS do BAM.

--- www/js/app.js
+++ www/js/app.js
@@ -6,7 +6,8 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers', 'ngCordova', 'angularMoment', 'n3-pie-chart'])
         .constant('Config', {
-            url: 'http://bptech.com.br/',
+            url: 'http://45.55.69.61/bam/',
+            //url: 'http://bptech.com.br/',
             //url: 'http://bptech.web1611.kinghost.net/',
             api: 'api/',
             userLogin: 'admin',
